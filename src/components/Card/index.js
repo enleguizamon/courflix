afc7e89de06ef4ce7fe34b3f2f326887f06464ef
Eli <eliana.leguizamon@gmail.com>
2020-08-29 23:19:52 -0300

    added progress bar

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -5,13 +5,27 @@ import { Link } from 'react-router-dom';
 class Card extends React.Component {
   render() {
     const { elem } = this.props;
-    return (
-      <div className="cardWrapper">
-        <Link to={`/watch/${elem.id}`}>
-        <img className="img" src={elem.img} />
-        </Link>
-      </div>
-    );
+    let bar = {width: elem.seen }
+
+    if (elem.seen) {
+      return (
+        <div className="cardWrapper">
+          <Link to={`/watch/${elem.id}`}>
+            <img className="img" src={elem.img} />
+          </Link>
+          <div className="progressBar">
+          <div className="progress" style={bar} ></div>
+          </div>
+        </div>
+      );
+    } else {
+      return (
+        <div className="cardWrapper">
+          <Link to={`/watch/${elem.id}`}>
+            <img className="img" src={elem.img} />
+          </Link>
+        </div>
+      )} 
   }
 }
 
--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -6,7 +6,7 @@ class Carousel extends React.Component {
   render() {
     return (
       <div className="carouselWrapper">
-        <h3 className="carouselTitle">Series</h3>
+        <h3 className="carouselTitle">Continuar viendo</h3>
         <InnerCarousel data={this.props.data} filter={"series"} />
         <h3 className="carouselTitle">Películas</h3>
         <InnerCarousel data={this.props.data} filter={"pelis"} />
