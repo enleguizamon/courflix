4220bf7543aadcf8138fcb708655e84610d51ee3
Eli <eliana.leguizamon@gmail.com>
2020-08-30 23:18:50 -0300

    added responsive

--- src/components/EpisodesCarousel/index.js
+++ src/components/EpisodesCarousel/index.js
@@ -15,6 +15,12 @@ class EpisodesCarousel extends React.Component {
       variableWidth: true,
       slidesToShow: 4,
       slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 768, 
+          settings: 'unslick'
+        }
+      ]
     }
   }
 
